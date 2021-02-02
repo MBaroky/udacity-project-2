5203d18c517740702ec1716b46e77bff89167adc
MBaroky <m.baroky@gmail.com>
2021-02-02 13:42:14 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express'); // express
+const bodyParser = require('body-parser'); // body-parser
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = '1803';
+const listen = ()=>{
+    console.log(`server up and running at: ${port}`);
+}
+app.listen(port,listen);
